2ca677a3a975d3aa70caaaeff02e7a1ec8eb3582
AKshat2701 <akshatsri2710@gmail.com>
2026-02-10 22:02:50 +0530

    with while loop

--- 02_for_loop/02_whileLoop.js
+++ 02_for_loop/02_whileLoop.js
@@ -14,12 +14,92 @@
 
 // sum of Digit 
 
-let num = 848485
-let sum = 0 
-while(num > 0 )
+// let num = 848485
+// let sum = 0
+// while(num > 0 )
+// {
+//    let digit = num % 10
+//    sum = sum + digit;
+//    num = Math.floor(num/10)
+// }
+// console.log(sum)
+
+// num = 2344
+// let digit = num % 10
+// console.log(digit)
+
+
+
+// Print numbers 1 to N using while loop
+// let n = 10;
+
+// let i = 1 ;
+
+// while(i <= n )
+// {
+//     console.log(i)
+//     i++
+// }
+
+
+// Print even numbers from 1 to N
+// let n = 10 ;
+// let i = 2
+
+// while( i <= n)
+// {
+//     console.log(i);
+//     i+=2 ;
+// }
+
+//Reverse counting 
+
+// let n = 5;
+
+// while (n >= 1) {
+//   console.log(n);
+//   n--;
+// }
+
+
+// Sum of first N natural numbers
+// let n = 50;
+// let i = 0 
+// let sum = 0 
+
+
+// while (i <= n)
+// {
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum)
+
+
+// Count digits in a number
+
+// let n = 32534999
+// let count = 0
+
+// while( n > 0 )
+// {
+//     count++
+//     n = Math.floor(n/10)
+// }
+
+// console.log(count)
+
+
+// Reverse a number
+let n = 234234234
+
+let rev = 0 
+
+while(n > 0)
 {
-   let digit = num % 10
-   sum = sum + digit;
-   num = Math.floor(num/10)
+    let digit = n % 10 
+    rev = rev * 10 + digit
+    n = Math.floor(n/10)
+
 }
-console.log(sum)
+console.log(rev)
