be8da8c8928a1a68eaad4f9d5751e86ad1e6fafc
AKshat2701 <akshatsri2710@gmail.com>
2026-02-17 07:11:48 +0530

    function call

---
+++ 04_function/01_add_two_number.js
@@ -0,0 +1,19 @@
+
+
+function add(a,b)
+{
+    return a + b
+}
+
+console.log(add(32,88))
+
+
+const func_exp = function (a , b) {
+    return a + b
+}
+
+console.log(func_exp(889,443))
+
+
+const mul = (a, b) => a * b 
+console.log(mul(3,10))
