d1727d62ff977d94ecf79c2af525e7d6537b79dc
AKshat2701 <akshatsri2710@gmail.com>
2026-02-18 22:54:08 +0530

    strings

---
+++ 07_String_basics/01_strings.js
@@ -0,0 +1,30 @@
+let name = "Akshat"
+let city = 'Noida'
+let msg = `hello`
+
+// console.log(name[0])
+// console.log(name[1])
+
+
+
+
+let str1 = "hello"
+let str2 = new String("HELLO")
+
+
+let text = "hello world"
+console.log(text.length)
+
+
+console.log("".length)
+
+
+let str = "Javascript"
+console.log(str[4])
+
+// console.log(str.charAt(5))
+console.log(str[100])
+console.log(str.charAt(100))
+
+
+
---
+++ 07_String_basics/02_string_Method.js
@@ -0,0 +1,17 @@
+
+let str = "HellO WORLD , LeTs LeArN jS"
+
+console.log(str.charAt(1))
+console.log(str.at(-1))
+
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+console.log(str.indexOf("L"))
+console.log(str.lastIndexOf("r"))
+console.log(str.includes("LeTs"))
+
+console.log(str.startsWith)
+
+
+
