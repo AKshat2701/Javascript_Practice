b399a1d30962aa803019861642f18d2f517d2987
AKshat2701 <akshatsri2710@gmail.com>
2026-03-05 11:03:06 +0530

    Ride booking details

---
+++ 10_Object_practice/10 Ride Booking app.js
@@ -0,0 +1,50 @@
+
+
+const details =
+{
+    drive : 
+    {
+        drivername : "raju driver",
+        rating : 4.5,
+        carname : "WagonR"
+    },
+    passenger :
+    {
+        passengername : "Hero alom",
+        passengerage : 43
+    },
+    pickuplocation : "park ultima sitapur road",
+
+    droplocation : "charbhag railway station",
+
+    fareperkm : 9.1 ,
+
+    distanceinKm : 30 ,
+
+    startRide()
+    {
+        const fare = this.calculateFare()
+        console.log(`The Ride is started and the fare is ${fare}`)
+    },
+
+    endRide()
+    {
+        
+        console.log(`You've reached the distination`)
+    },
+
+    calculateFare()
+    {       
+        console.log("Total fare cost is")
+        return this.fareperkm * this.distanceinKm
+    }
+}
+
+
+console.log(details)
+
+console.log(details.calculateFare())
+
+details.startRide()
+details.endRide()
+
