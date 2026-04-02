1733c3123e8bfc97bce171778aad458f6b171ba9
AKshat2701 <akshatsri2710@gmail.com>
2026-04-03 00:16:39 +0530

    js practice question

---
+++ practice/01.js
@@ -0,0 +1,49 @@
+// function add(a, b)
+// {
+//     console.log(a + b)
+// }
+
+
+// add(32,44)
+
+// const add = (a , b) => a * b
+
+// console.log(add(32,12))
+
+
+// const square = x => x * x
+
+// console.log(square(43))
+
+// const greet = () => "Hello"
+
+// console.log(greet())
+
+
+// const getUser = () => ({name : "Akshat"})
+
+// console.log(getUser())
+
+
+// for of loop 
+// const num = [10,20,30,40]
+
+// for(let item of num)
+// {
+//     console.log(item + 10)
+// }
+
+// const name = "Akshat"
+
+// for(let char of name)
+// {
+//     console.log(char)
+// }
+
+// const arr = ["a", "b" , "c"]
+
+
+// for(let [i , v ] of arr.entries())
+// {
+//     console.log(i , v)
+// }
