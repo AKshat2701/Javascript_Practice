ae17f100db620b13e9d04d5568b96be5d44486bb
AKshat2701 <akshatsri2710@gmail.com>
2026-04-09 11:37:31 +0530

    Basic set and map and methods

---
+++ 14_Map_and_Set/01_set.js
@@ -0,0 +1,22 @@
+const myarr = [1,2,3,4,5,6,7,8,9,10]
+
+const myset = new Set(myarr)
+
+
+myset.add(11);
+myset.add("11");
+myset.add({channelName : "JS mastery"})
+myset.add([1,2,3])
+console.log(myset)
+
+
+// myset.delete(3)
+// myset.clear()
+
+// console.log(myset)
+// console.log(myset.has(2))
+
+// console.log(myset.size)
+
+// console.log(myset[3])
+
---
+++ 14_Map_and_Set/02_map.js
@@ -0,0 +1,19 @@
+// const mymap = new Map([ ["name" , "john"], ["surname" , "Doe"]])
+
+// console.log(mymap)
+
+
+
+// const myObj = {}
+
+const mymap = new Map([[1,"one"], [2, "two"]]);
+
+// mymap.delete(1)
+// mymap.clear()
+console.log(mymap.has(1))
+// console.log(mymap)
+
+console.log(mymap.size)
+
+
+
