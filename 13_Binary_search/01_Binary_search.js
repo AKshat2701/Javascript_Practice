fab2017c4588fbea6bbd2b7e29aea70d6c0ed693
AKshat2701 <akshatsri2710@gmail.com>
2026-04-09 11:36:58 +0530

    Binary serach code

--- 13_Binary_search/01_Binary_search.js
+++ 13_Binary_search/01_Binary_search.js
@@ -1,5 +1,28 @@
-const binarySearch = (nums) =>
+const binarySerach =(nums, target) =>
 {
-    let start = 0
-    let end = nums.length 
+    let start = 0 
+    let end = nums.length - 1 
+
+
+    let middle = Math.floor((start + end) / 2)
+
+    while(start <= end)
+    {
+        let middle = Math.floor((start+end) / 2)
+
+        if(nums[middle] === target)
+        {
+            return middle ;
+        } else if(nums[middle] < target)
+        {
+            start = middle + 1 
+        }
+        else {
+            end = middle - 1 ; 
+        }
+    }
+
+    return -1 
 }
+
+console.log(binarySerach([-2,1,2,4,7,8,9,10], 10))
