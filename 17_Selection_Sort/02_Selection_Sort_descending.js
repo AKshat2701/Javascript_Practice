cbc8527a60053051fc98b3c26f327a7b13720b30
AKshat2701 <akshatsri2710@gmail.com>
2026-04-11 01:36:03 +0530

    selection sort

---
+++ 17_Selection_Sort/02_Selection_Sort_descending.js
@@ -0,0 +1,23 @@
+
+const selectionSort = (arr) =>
+{
+    for(let i = 0 ; i < arr.length - 1 ; i++)
+    {
+        let minIdx = i ;
+
+        for(let j = i + 1 ; j < arr.length ; j++)
+        {
+            if(arr[j] > arr[minIdx])
+            {
+                minIdx = j ; 
+            }
+        }
+        if(minIdx !== i)
+        {
+            const temp = arr[i]
+            arr[i] = arr[minIdx]
+            arr[minIdx] = temp ; 
+        }
+    }
+    return arr ; 
+}
