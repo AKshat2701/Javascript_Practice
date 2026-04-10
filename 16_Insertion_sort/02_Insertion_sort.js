4c998188c950bfa9372047db0c9943f051bbcdfa
AKshat2701 <akshatsri2710@gmail.com>
2026-04-10 20:05:43 +0530

    Insertion sort with javascript

---
+++ 16_Insertion_sort/02_Insertion_sort.js
@@ -0,0 +1,18 @@
+const insertionSort = (arr) =>
+{
+    for(let i = 1 ; i < arr.length ; i++)
+    {
+        let curr = arr[i]
+        let j = i - 1 
+
+        while( j >= 0 && arr[j] > curr)
+        {
+            arr[j+1] = arr[j]
+            j--;
+        }
+        arr[j + 1] = curr ; 
+    }
+    return arr ; 
+}
+
+console.log(insertionSort([943,56,34,2, 6,34, 6342,68]))
