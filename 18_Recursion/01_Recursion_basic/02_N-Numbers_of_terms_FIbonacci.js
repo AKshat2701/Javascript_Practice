14331ed8b186b4ee521f38efc60d684743301c9d
AKshat2701 <akshatsri2710@gmail.com>
2026-04-12 16:06:38 +0530

    N Numbers of terms using fibonacci

---
+++ 18_Recursion/01_Recursion_basic/02_N-Numbers_of_terms_FIbonacci.js
@@ -0,0 +1,15 @@
+let prompt = require("prompt-sync")()
+let n = Number(prompt("Enter a number"))
+
+
+
+let first = 0 , second =  1; 
+
+for(let i = 1 ;  i <= n - 2 ; i++)
+{
+    let third = first + second 
+    process.stdout.write(third + " ")
+    first = second
+    second = third ; 
+}
+
