fdcd8270c95a8d4c7dcd92bed7f848a6c152b744
AKshat2701 <akshatsri2710@gmail.com>
2026-04-14 22:51:47 +0530

    2D array traversing

---
+++ 19_2D_arrays/01_2D_array.js
@@ -0,0 +1,19 @@
+const matrix = 
+[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+
+// console.log(matrix.length)
+
+
+
+for(let i = 0 ; i < matrix.length ; i++)
+{
+    for(let j = 0 ;j < matrix[i].length ; j++)
+    {
+        console.log(matrix[i][j])
+    }
+}
