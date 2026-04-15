cf584e4ffa8b05ff989ab534aee2d0fcdadc8597
AKshat2701 <akshatsri2710@gmail.com>
2026-04-15 09:19:15 +0530

    2D array traversing

--- 19_2D_arrays/01_2D_array.js
+++ 19_2D_arrays/01_2D_array.js
@@ -1,19 +1,49 @@
-const matrix = 
-[
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,16]
-]
+// const matrix = 
+// [
+//     [1,2,3,4],
+//     [5,6,7,8],
+//     [9,10,11,12],
+//     [13,14,15,16]
+// ]
 
 // console.log(matrix.length)
 
 
+//row wise
+// for(let i = 0 ; i < matrix.length ; i++)
+// {
+//     for(let j = 0 ;j < matrix[i].length ; j++)
+//     {
+//         console.log(matrix[i][j])
+//     }
+// }
 
-for(let i = 0 ; i < matrix.length ; i++)
+
+let arr = 
+[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9] 
+]
+
+//row wise 
+// for(let i = 0 ; i < arr.length ; i++)
+// {
+//     for(let j = 0 ; j < arr[i].length ; j++)
+//     {
+//         process.stdout.write(arr[i][j] + " ")
+//     }
+//     console.log()
+// }
+
+
+// col wise
+console.log("Printing column wise ") 
+for(let i = 0 ; i< arr.length ; i++)
 {
-    for(let j = 0 ;j < matrix[i].length ; j++)
+    for(let j = 0 ; j < arr[i].length ; j++)
     {
-        console.log(matrix[i][j])
+        process.stdout.write(arr[j][i] +  " ")
     }
+    console.log()
 }
