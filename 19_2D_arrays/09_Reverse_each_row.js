8a961d0dee02b6e73d83a368b92959d162a3564e
AKshat2701 <akshatsri2710@gmail.com>
2026-04-16 22:06:53 +0530

    Reverse row wise

---
+++ 19_2D_arrays/09_Reverse_each_row.js
@@ -0,0 +1,19 @@
+
+let arr = 
+[
+    [2,12,3,4],
+    [84,3,29,9],
+    [78,4,34,9],
+    [89,4,87,8]
+]
+
+const reverseRow = (nums) =>
+{
+    for(let i = 0 ; i < nums.length ; i++)
+    {
+        for(let j = 0 ; j < nums.length ; j++)
+        {
+            
+        }
+    }
+}
