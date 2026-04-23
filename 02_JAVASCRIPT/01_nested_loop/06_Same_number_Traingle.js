569d01bfa1cfb842d56f2f21e13ca0231114c5d2
AKshat2701 <akshatsri2710@gmail.com>
2026-04-23 16:40:55 +0530

    Pyramid pattern

---
+++ 02_JAVASCRIPT/01_nested_loop/06_Same_number_Traingle.js
@@ -0,0 +1,17 @@
+let n = 5 ; 
+
+for(let i = 0 ; i < n ; i++)
+{
+    let row = ""
+    for(let j = 0 ; j <= i ; j++)
+    {
+        row = row + i + " "
+    }
+    console.log(row)
+}
+
+// 0 
+// 1 1 
+// 2 2 2 
+// 3 3 3 3 
+// 4 4 4 4 4 
