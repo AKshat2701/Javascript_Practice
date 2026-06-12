9c347e0dc9e5d7ca1944ebafc4610962d999c2d6
AKshat2701 <akshatsri2710@gmail.com>
2026-06-13 00:30:36 +0530

    HOF and Map

---
+++ 04_JAVASCRIPT/02_MAP/01_map.js
@@ -0,0 +1,50 @@
+// const number = [1,2,3,4,5]
+
+// const multipleOfTwo = number.map((item)=>
+// {
+//     return item * 2
+// })
+
+// console.log(multipleOfTwo)
+
+
+
+// const squares = number.map((item) =>
+// {
+//     return item * item
+// })
+
+// console.log(squares)
+
+
+// const cube = number.map((item) =>
+// {
+//     return item * item * item 
+// })
+
+// console.log(cube)
+
+
+
+// const num = [2,4,6]
+
+// const double = num.map((i) =>
+// {
+//     return i * 2 
+// })
+
+// console.log(double)
+
+
+const obj = 
+[
+    {name : "Akshat"},
+    {name : "Sara"},
+    {name : "TOM"}
+]
+
+const name = obj.map((item) =>
+{
+    return item.name
+})
+console.log(name)
