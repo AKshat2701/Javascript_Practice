76e1a13ae5a33697ea91fe4285b9628e6818779d
AKshat2701 <akshatsri2710@gmail.com>
2026-07-02 23:57:01 +0530

    Changes in selection sort

--- 03_JAVASCRIPT/02_SORT/01_selection_sort/01_Selection_sort.js
+++ 03_JAVASCRIPT/02_SORT/01_selection_sort/01_Selection_sort.js
@@ -1,19 +1,19 @@
 let arr = [ 5,3,7,9,1, 4]
 
-
 function selectionSort(arr)
 {
     let n = arr.length ; 
 
-    for(let i = 0 ; i < n - 1 ; i++)
+    for(let i = 0 ; i < n - 1 ; i++ )
     {
+        
         let minIndex = i ; 
 
         for(let j = i + 1 ; j < n ; j++)
         {
             if(arr[minIndex] > arr[j])
             {
-                minIndex = j 
+                minIndex = j ; 
             }
         }
         if(minIndex != i)
@@ -25,6 +25,5 @@ function selectionSort(arr)
     }
     return arr ; 
 }
-
 
 console.log(selectionSort(arr))
