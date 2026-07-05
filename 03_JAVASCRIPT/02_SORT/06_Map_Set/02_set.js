2ce3cc257f73cde8fc37734e03ffdf1bfeb06daf
AKshat2701 <akshatsri2710@gmail.com>
2026-07-05 21:18:27 +0530

    Knowing hashmap and set in js

---
+++ 03_JAVASCRIPT/02_SORT/06_Map_Set/02_set.js
@@ -0,0 +1,5 @@
+let set = new Set([1,2,3,4,5,6,1,2,3,4,5])
+
+let arr = [...set]
+
+console.log(arr)
---
+++ 03_JAVASCRIPT/02_SORT/06_Map_Set/03_Map.js
@@ -0,0 +1,6 @@
+let map = new Map()
+
+map.set("name" , "Akshat")
+map.set("age", 24)
+
+console.log(map)
