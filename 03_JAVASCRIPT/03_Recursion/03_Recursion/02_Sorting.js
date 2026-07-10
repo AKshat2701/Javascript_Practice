c35074e56fc2c40cbbf94d1cc2168a7bb0614081
AKshat2701 <akshatsri2710@gmail.com>
2026-07-10 22:37:31 +0530

    sorting

---
+++ 03_JAVASCRIPT/03_Recursion/03_Recursion/02_Sorting.js
@@ -0,0 +1,29 @@
+function bubbleSort(arr)
+{
+    let n = arr.length ; 
+    
+
+    for(let i = 0 ; i < n - 1 ; i++)
+    {
+        let swapped = false ;
+        for(let j = 0 ; j < n - 1 - i ; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                let temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp ; 
+
+                swapped = true; 
+            }
+
+        }
+        if(!swapped) {
+            break 
+        }
+    }
+
+    return arr ;
+}
+let arr = [9,4,5,2,7,1,8]
+console.log(bubbleSort(arr))
