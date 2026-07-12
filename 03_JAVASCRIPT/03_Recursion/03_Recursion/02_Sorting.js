e07ba778c8d06b093932f754a150398db4638ec8
AKshat2701 <akshatsri2710@gmail.com>
2026-07-12 19:36:34 +0530

    practice

--- 03_JAVASCRIPT/03_Recursion/03_Recursion/02_Sorting.js
+++ 03_JAVASCRIPT/03_Recursion/03_Recursion/02_Sorting.js
@@ -131,23 +131,48 @@
 
 
 //Insertion Sort 
+// function insertionSort(arr)
+// {
+//    let n = arr.length ; 
+//    for(let i = 1 ; i < n ; i++) 
+//    {
+//     let key = arr[i]
+//     let j = i - 1 ;
+
+//     while( j >= 0 && arr[j] > key)
+//     {
+//         arr[ j + 1] = arr[j]
+//         j-- ; 
+//     }
+//     arr[j + 1] = key ; 
+//    }
+//    return arr ; 
+// }
+
+// let arr = [8,4,2,1,6,7,12,90]
+// console.log(insertionSort(arr))
+
+
+//Insertion Sort 
 function insertionSort(arr)
 {
-   let n = arr.length ; 
-   for(let i = 1 ; i < n ; i++) 
-   {
-    let key = arr[i]
-    let j = i - 1 ;
+    let n = arr.length ; 
 
-    while( j >= 0 && arr[j] > key)
+    for(let i = 1 ; i < n ; i++)
     {
-        arr[ j + 1] = arr[j]
-        j-- ; 
+        let key = arr[i]
+        let j = i - 1 ; 
+
+        while( j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j]
+            j-- ; 
+        }
+        arr[j + 1] = key ; 
     }
-    arr[j + 1] = key ; 
-   }
-   return arr ; 
+    return arr ; 
 }
 
-let arr = [8,4,2,1,6,7,12,90]
+
+let arr = [9,10,8,7,6,4,1,5,3]
 console.log(insertionSort(arr))
