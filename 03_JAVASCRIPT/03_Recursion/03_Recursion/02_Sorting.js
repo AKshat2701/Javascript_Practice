68a6d842106bcada2ce1828c509b9c04872eb57a
AKshat2701 <akshatsri2710@gmail.com>
2026-07-12 10:52:41 +0530

    practice sorting

--- 03_JAVASCRIPT/03_Recursion/03_Recursion/02_Sorting.js
+++ 03_JAVASCRIPT/03_Recursion/03_Recursion/02_Sorting.js
@@ -1,29 +1,153 @@
-function bubbleSort(arr)
-{
-    let n = arr.length ; 
+// function bubbleSort(arr)
+// {
+//     let n = arr.length ; 
     
 
-    for(let i = 0 ; i < n - 1 ; i++)
+//     for(let i = 0 ; i < n - 1 ; i++)
+//     {
+//         let swapped = false ;
+//         for(let j = 0 ; j < n - 1 - i ; j++)
+//         {
+//             if(arr[j] > arr[j + 1])
+//             {
+//                 let temp = arr[j]
+//                 arr[j] = arr[j + 1]
+//                 arr[j + 1] = temp ; 
+
+//                 swapped = true; 
+//             }
+
+//         }
+//         if(!swapped) {
+//             break 
+//         }
+//     }
+
+//     return arr ;
+// }
+
+// function bubbleSort(arr)
+// {
+//     let n = arr.length ; 
+//     for(let i = 0 ; i < n ; i++)
+//     {
+//         let swapped = false ; 
+//         for(let j = 0 ; j < n - i - 1 ; j++)
+//         {
+//             if(arr[j] > arr[j + 1])
+//             {
+//                 let temp = arr[j]
+//                 arr[j] = arr[j + 1]
+//                 arr[j + 1] = temp ; 
+
+//                 swapped = true ;
+//             }
+//         }
+//         if(!swapped)
+//         {
+//             break ;
+//         }
+
+//     }
+//     return arr ; 
+// }
+
+// let arr = [9,5,2,1,7,10,8]
+// console.log(bubbleSort(arr))
+
+
+
+
+//Selection Sort 
+// function selectionSort(arr)
+// {
+//     let n = arr.length ; 
+
+//     for(let i = 0 ; i < n ; i++)
+//     {
+//         let minIndex = i ; 
+
+//         for(let j = i + 1; j < n ; j++)
+//         {
+//             if(arr[j] < arr[minIndex])
+//             {
+//                 minIndex = j ; 
+//             }
+//         }
+//         if(minIndex != i)
+//         {
+//             let temp = arr[minIndex]
+//             arr[minIndex] = arr[i]
+//             arr[i] = temp ;
+//         }
+//     }
+//     return arr ; 
+// }
+
+// let arr = [9,2,8,4,1,43,85]
+
+// console.log(selectionSort(arr))
+
+
+// function selectionSort(arr)
+// {
+//     let n = arr.length ; 
+
+//     for(let i = 0 ; i < n ; i++)
+//     {
+//         let minIndex = i ; 
+
+//         for(let j = i + 1; j < n ; j++)
+//         {
+//             if(arr[j] < arr[minIndex])
+//             {
+//                 minIndex = j ; 
+//             }
+//         }
+//         if(minIndex != i)
+//         {
+//             let temp = arr[minIndex]
+//             arr[minIndex] = arr[i]
+//             arr[i] = temp ; 
+//         }
+//     }
+//     return arr ;
+// }
+// let arr = [9,4,2,7,1,7,8,18]
+
+// console.log(selectionSort(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Insertion Sort 
+function insertionSort(arr)
+{
+   let n = arr.length ; 
+   for(let i = 1 ; i < n ; i++) 
+   {
+    let key = arr[i]
+    let j = i - 1 ;
+
+    while( j >= 0 && arr[j] > key)
     {
-        let swapped = false ;
-        for(let j = 0 ; j < n - 1 - i ; j++)
-        {
-            if(arr[j] > arr[j + 1])
-            {
-                let temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp ; 
-
-                swapped = true; 
-            }
-
-        }
-        if(!swapped) {
-            break 
-        }
+        arr[ j + 1] = arr[j]
+        j-- ; 
     }
-
-    return arr ;
+    arr[j + 1] = key ; 
+   }
+   return arr ; 
 }
-let arr = [9,4,5,2,7,1,8]
-console.log(bubbleSort(arr))
+
+let arr = [8,4,2,1,6,7,12,90]
+console.log(insertionSort(arr))
